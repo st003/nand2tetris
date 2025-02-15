6799f6e0811137faba05a9e74db77b2d8d77f659
st003 <stuart.j.calcote@gmail.com>
2025-02-14 19:56:21 -0700

    Added file openin logic

--- projects/6/HackAssembler/main.go
+++ projects/6/HackAssembler/main.go
@@ -11,10 +11,18 @@ func main() {
 	args := os.Args
 
 	if len(args) != 2 {
-		fmt.Println("Usage: ha asm_file")
+		fmt.Println("Usage: ha asmfile")
 		os.Exit(0)
 	}
 
-	fmt.Printf("Assembling %v ...\n", args[1])
-	os.Exit(0)
+	asmFilePath := args[1]
+
+	file, err := os.Open(asmFilePath)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	fmt.Printf("Assembling %v\n", asmFilePath)
 }

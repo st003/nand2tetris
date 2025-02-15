0469adbfbd4034940a74538ba05065115fac3219
st003 <stuart.j.calcote@gmail.com>
2025-02-14 19:40:30 -0700

    Added usage statement

--- projects/6/HackAssembler/main.go
+++ projects/6/HackAssembler/main.go
@@ -1,8 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 
 func main() {
-	fmt.Printf("Hello, Assembler!\n")
+
+	args := os.Args
+
+	if len(args) != 2 {
+		fmt.Println("Usage: ha asm_file")
+		os.Exit(0)
+	}
+
+	fmt.Printf("Assembling %v ...\n", args[1])
+	os.Exit(0)
 }

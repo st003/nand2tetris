5a222ccbc1bd7a228fd490cbfc938bdd80660854
st003 <stuart.j.calcote@gmail.com>
2025-02-16 13:46:53 -0700

    Project 6 assembler now produces an outfile

--- projects/6/HackAssembler/main.go
+++ projects/6/HackAssembler/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"time"
 	"os"
+	"strings"
+	"time"
 )
-
 
 func main() {
 
@@ -25,11 +26,32 @@ func main() {
 	}
 	defer inFile.Close()
 
+	lines := make([]string, 0, 10)
+
+	scanner := bufio.NewScanner(inFile)
+	for scanner.Scan() {
+		line := scanner.Text()
+		lines = append(lines, line)
+	}
+
+	outFile, err := os.Create("outfile.hack")
+	if err != nil {
+		fmt.Println("Error opening outfile.hack:", err)
+		os.Exit(1)
+	}
+	defer outFile.Close()
+
+	_, err = outFile.WriteString(strings.Join(lines, "\n"))
+	if err != nil {
+		fmt.Println("Unable to write lines to outfile", err)
+		os.Exit(2)
+	}
+
 	start := time.Now()
 
 	fmt.Printf("Assembling %v\n", asmFilePath)
 
 	end := time.Now()
-	executionTime := float64(end.UnixMilli() - start.UnixMilli()) / float64(1000)
+	executionTime := float64(end.UnixMilli()-start.UnixMilli()) / float64(1000)
 	fmt.Printf("\nDone. Assembly time: %v seconds\n", executionTime)
 }

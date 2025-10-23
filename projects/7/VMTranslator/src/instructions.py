169e7eca686450bd4a338a1e98ea045027b70430
st003 <stuart.j.calcote@gmail.com>
2025-10-22 18:07:52 -0700

    VMTranslator: added constant as method to PushInstruction

--- projects/7/VMTranslator/src/instructions.py
+++ projects/7/VMTranslator/src/instructions.py
@@ -16,7 +16,7 @@ class AddInstruction(BaseInstruction):
             '@SP',
             'AM=M-1', # deincrement stack-pointer & select new stack location
             'D=M', # copy value at stack-pointer
-            'A=A-1', # select position stack-pointer - 1
+            'A=A-1', # select position below stack-pointer
             'M=D+M' # update stack with sum of both values
         ]
 
@@ -28,12 +28,15 @@ class SubInstruction(BaseInstruction):
             '@SP',
             'AM=M-1', # deincrement stack-pointer & select new stack location
             'D=M', # copy value at stack-pointer
-            'A=A-1', # select position stack-pointer - 1
+            'A=A-1', # select position below stack-pointer
             'M=M-D' # update stack with difference of both values
         ]
 
 class NegInstruction(BaseInstruction):
+    # TODO: zero minus value should flip the sign
     pass
+
+# TODO: numerical values represent true/false. Has to be @1 and @0, right?
 
 class EqInstruction(BaseInstruction):
     pass
@@ -43,6 +46,8 @@ class GtInstruction(BaseInstruction):
 
 class LtInstruction(BaseInstruction):
     pass
+
+# TODO: should just be able to use the &, |, ! operations
 
 class AndInstruction(BaseInstruction):
     pass
@@ -56,16 +61,15 @@ class NotInstruction(BaseInstruction):
 # memory instructions
 
 class PushInstruction(BaseInstruction):
-
+    """Generates the Hack ASM for a push command."""
     def __init__(self, line_num, parts):
         self._line_num = line_num
         self._parts = parts
 
         self._asm = [
             self.get_comment(),
-            # TODO: this works only with constant
-            f'@{parts[2]}', # select constant value
-            'D=A',
+            self.get_constant(),
+            # This asm is the same for memory segements
             '@SP', # select top of stack
             'A=M',
             'M=D', # set constant value to top of stack
@@ -74,7 +78,16 @@ class PushInstruction(BaseInstruction):
         ]
 
     def get_comment(self):
+        """Generates a comment line of the original VM instruction."""
         return '// ' + ' '.join(self._parts)
+
+    def get_constant(self):
+        """Selects a constant value."""
+        asm = [
+            f'@{self._parts[2]}',
+            'D=A'
+        ]
+        return '\n'.join(asm)
 
 class PopInstruction(BaseInstruction):
     pass

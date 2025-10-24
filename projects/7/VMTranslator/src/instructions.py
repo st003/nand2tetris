6933cee9c1a89c84d26babd40d59cce4a3e40b1c
st003 <stuart.j.calcote@gmail.com>
2025-10-23 18:50:59 -0700

    VMTranslator: added support for static memory segement in pop command

--- projects/7/VMTranslator/src/instructions.py
+++ projects/7/VMTranslator/src/instructions.py
@@ -129,7 +129,42 @@ class PushInstruction(MemoryInstruction):
         elif seg == 'constant':
             return self.get_constant()
         else:
-            raise TranslationError(f'Memory segement "{seg}" not recognized')
+            raise TranslationError(f'Error at line {self._line_num}. Memory segement "{seg}" not recognized')
 
 class PopInstruction(MemoryInstruction):
-    pass
+    """Generates the Hack ASM for a pop command."""
+
+    def __init__(self, line_num, parts):
+        self._line_num = line_num
+        self._parts = parts
+
+        self._asm = [
+            self.get_comment(),
+            '@SP',
+            'AM=M-1', # deincrement stack-pointer & select new stack location
+            'D=M', # make copy of selected value in the stack
+            self.set_value_in_segment()
+        ]
+
+    def set_static(self) -> str:
+        """
+        Get address for static memory segment.
+
+        The specification says static occupies addresses 16-255, but as
+        the 0-index for the stack is implementation specific, we do not
+        have static-overflow checking
+        """
+        asm = [
+            f'@static.{self.get_offset()}', # create asm variable called "static.i" (and selected it)
+            'M=D' # get the value stored at that address
+        ]
+        return '\n'.join(asm)
+
+    def set_value_in_segment(self):
+        """Selects the correct segment asm method."""
+        seg = self.get_memory_segment()
+
+        if seg == 'static':
+            return self.set_static()
+        else:
+            raise TranslationError(f'Error at line {self._line_num}. Memory segement "{seg}" not recognized')

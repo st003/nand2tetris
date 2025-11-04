e32b7127bbbce2814e6a6396adeb40e9fef46af5
st003 <stuart.j.calcote@gmail.com>
2025-11-03 19:22:27 -0700

    VMTranslator: added a performance counter

--- projects/8/VMTranslator/src/VMTranslator.py
+++ projects/8/VMTranslator/src/VMTranslator.py
@@ -1,4 +1,5 @@
 import sys
+import time
 import traceback
 
 from exceptions import VMTranslatorError
@@ -14,6 +15,8 @@ def main():
         sys.exit(1)
 
     try:
+        start_time = time.perf_counter()
+
         input_file_path = args[1]
 
         parent_dirs, file_name = get_vm_file_name(input_file_path)
@@ -24,8 +27,15 @@ def main():
 
         asm_lines = translate(input_lines)
 
-        with open(f'{parent_dirs}/{file_name}.asm', 'w') as asm_file:
+        output_file_path = f'{parent_dirs}/{file_name}.asm'
+        with open(output_file_path, 'w') as asm_file:
             asm_file.writelines(asm_lines)
+
+        end_time: float = time.perf_counter()
+        exec_time: float = round((end_time - start_time), 5)
+
+        print(f'\nTranslation complete. Output asm exported to: {output_file_path}')
+        print(f'Execution time: {exec_time} seconds\n')
 
     except VMTranslatorError as error:
         print(f'Error: {error}')

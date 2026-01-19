c318ff4c5d4f70f205880ce92ffdc09631d7329d
st003 <stuart.j.calcote@gmail.com>
2026-01-18 22:53:18 -0700

    Project 11: scafolded SymbolTable class

---
+++ projects/11/JackCompiler/src/SymbolTable.py
@@ -0,0 +1,32 @@
+# TODO: api implementation def found at: 5.10 @17:00
+
+class SymbolTable():
+
+    def __init__(self):
+        # TODO: implement
+        # initialize class-level symbol table
+        pass
+
+    def startSubroutine(self):
+        # TODO: implement
+        pass
+
+    def define(self):
+        # TODO: implement
+        pass
+
+    def VarCount(self):
+        # TODO: implement
+        pass
+
+    def KindOf(self):
+        # TODO: implement
+        pass
+
+    def TypeOf(self):
+        # TODO: implement
+        pass
+
+    def IndexOf(self):
+        # TODO: implement
+        pass
